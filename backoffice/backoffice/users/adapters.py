a5bc7dc3a185c962145921c7b36f9b32ac6b7bc8
DonHaul <ramiro.animus@gmail.com>
2024-09-20 14:05:49 +0200

    login done, but no email

--- backoffice/backoffice/users/adapters.py
+++ backoffice/backoffice/users/adapters.py
@@ -24,20 +24,33 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
     ) -> bool:
         return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
     
+    import urllib.parse
     from django.http import HttpResponseRedirect
     def pre_social_login(self, request, sociallogin):
         print("PRE SOCIAL SIGNUP")
         print(str(request.user))
-        print(str(sociallogin.user))
+        print(str(request))
+        print(request.META)
+        print(sociallogin.serialize())
 
+    #     import urllib
+    #     email = sociallogin.account.extra_data.get('email')
+    # # If email is missing, serialize the social login data and pass it to Flask
+    #     if not email:
+    #         # Serialize the social login data
+    #         sociallogin_data = sociallogin.serialize()
+    #         session_token = request.session.session_key  # Optional: Store session key for further authentication
 
-        # Extract email from the social login data
-        email = sociallogin.account.extra_data.get('email')
-        
-        # If email is missing, redirect to the 'fill-email' page
-        if not email:
-            request.session['sociallogin'] = sociallogin.serialize()  # Store social login data in session
-            return HttpResponseRedirect(reverse('fill_email')) 
+    #         # URL encode the social login data to send it securely to Flask
+    #         flask_redirect_url = 'http://localhost:5000/fill-email'
+    #         query_params = {
+    #             'sociallogin': urllib.parse.quote(sociallogin_data),
+    #             'session_token': session_token
+    #         }
+
+    #         url_with_params = f"{flask_redirect_url}?{urllib.parse.urlencode(query_params)}"
+    #         return HttpResponseRedirect(url_with_params)
+
 
     def populate_user(
         self,
